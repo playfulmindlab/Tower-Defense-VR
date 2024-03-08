bb95338d9fb3d8fe48030633456d2bf05436d46c
gabwilds <156127166+gabwilds@users.noreply.github.com>
2024-03-08 12:04:29 -0500

    Updated BalanceBoard Values
    -when reading in Balance Board values, we now get the values as an array of floats, which we can easily interpret as values for balancing - will convert these values into rotations in the near future

--- Assets/Scripts/Balance Board/SimpleInletBalanceBoard.cs
+++ Assets/Scripts/Balance Board/SimpleInletBalanceBoard.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LSL;
+using System;
 
 namespace LSL4Unity.Samples.SimpleInlet
 {
@@ -26,8 +27,9 @@ namespace LSL4Unity.Samples.SimpleInlet
         private StreamInlet inlet;
 
         // We need buffers to pass to LSL when pulling data.
-        private float[,] data_buffer;  // Note it's a 2D Array, not array of arrays. Each element has to be indexed specifically, no frames/columns.
-        private double[] timestamp_buffer;
+        private string[] data_buffer;  // Note it's a 2D Array, not array of arrays. Each element has to be indexed specifically, no frames/columns.
+        private double timestamp_buffer;
+
 
         void Start()
         {
@@ -44,23 +46,31 @@ namespace LSL4Unity.Samples.SimpleInlet
 
         IEnumerator ResolveExpectedStream()
         {
+
             var results = resolver.results();
             while (results.Length == 0)
             {
+                Debug.Log("Detecting results");
                 yield return new WaitForSeconds(.1f);
                 results = resolver.results();
             }
 
+            Debug.Log("Found results : " + results.Length);
             inlet = new StreamInlet(results[0]);
+
+            Debug.Log(inlet.ToString() + " // " + inlet.info().type());
 
             // Prepare pull_chunk buffer
             int buf_samples = (int)Mathf.Ceil((float)(inlet.info().nominal_srate() * max_chunk_duration));
-            // Debug.Log("Allocating buffers to receive " + buf_samples + " samples.");
-            int n_channels = inlet.info().channel_count();
-            data_buffer = new float[buf_samples, n_channels];
-            timestamp_buffer = new double[buf_samples];
+            Debug.Log("Allocating buffers to receive " + buf_samples + " samples.");
 
-            //inlet.open_stream
+            data_buffer = new string[1];
+            timestamp_buffer = 0.0;
+        }
+
+        private void OnApplicationQuit()
+        {
+            inlet.Close();
         }
 
         // Update is called once per frame
@@ -68,28 +78,58 @@ namespace LSL4Unity.Samples.SimpleInlet
         {
             if (inlet != null)
             {
-                Debug.Log(inlet.ToString());
-                //Debug.Log(inlet.)
-                int samples_returned = inlet.pull_chunk(data_buffer, timestamp_buffer);
-                Debug.Log("Samples returned: " + samples_returned);
+                inlet.pull_sample(data_buffer, timestamp_buffer);
 
-                if (samples_returned > 0)
+                Debug.Log("Samples returned: " + data_buffer[0] + " // Data Buffer: " + data_buffer.Length + " // Timestamps: " + timestamp_buffer);
+
+                if (data_buffer[0] != null)
                 {
-                    // There are many things you can do with the incoming chunk to make it more palatable for Unity.
-                    // Note that if you are going to do significant processing and feature extraction on your signal,
-                    // it makes much more sense to do that in an external process then have that process output its
-                    // result to yet another stream that you capture in Unity.
-                    // Most of the time we only care about the latest sample to get a visual representation of the latest
-                    // state, so that's what we do here: take the last sample only and use it to udpate the object scale.
-                    float x = data_buffer[samples_returned - 1, 0];
-                    float y = data_buffer[samples_returned - 1, 1];
-                    float z = data_buffer[samples_returned - 1, 2];
-                    var new_scale = new Vector3(x, y, z);
-                    // Debug.Log("Setting cylinder scale to " + new_scale);
-                    //gameObject.transform.localScale = new_scale;
-                    Debug.Log(new_scale.ToString());
+                    Debug.Log("Convert String to Viable Code here");
+
+                    float[] coordValues = ConvertStringToFloat(data_buffer[0].ToString());
+
+                    Debug.Log("Maggies: " + coordValues[2]);
+
+                    //string[] splitData = dataString.Split(dataString, char.Parse("X: "));
                 }
+                //if (samples_returned > 0)
+                //{
+                // There are many things you can do with the incoming chunk to make it more palatable for Unity.
+                // Note that if you are going to do significant processing and feature extraction on your signal,
+                // it makes much more sense to do that in an external process then have that process output its
+                // result to yet another stream that you capture in Unity.
+                // Most of the time we only care about the latest sample to get a visual representation of the latest
+                // state, so that's what we do here: take the last sample only and use it to udpate the object scale.
+                // float x = data_buffer[samples_returned - 1, 0];
+                //float y = data_buffer[samples_returned - 1, 1];
+                //float z = data_buffer[samples_returned - 1, 2];
+                //var new_scale = new Vector3(x, y, z);
+                // Debug.Log("Setting cylinder scale to " + new_scale);
+                //gameObject.transform.localScale = new_scale;
+                //}
             }
+        }
+
+        float[] ConvertStringToFloat(string str)
+        {
+            string dataString = str.ToString();
+            string[] delimiters = new string[] { "COP Measurement -> X: ", "Y: ", "Mag: " };
+            string[] splitData = dataString.Split(delimiters, StringSplitOptions.None);
+
+            float[] values = new float[3];
+
+            if (splitData.Length > 3 && float.TryParse(splitData[1], out float result1) && float.TryParse(splitData[2], out float result2))
+            {
+                //NOTE: splitData[] is only "", and is ignored when recording values
+                Debug.Log("Split Data: " + splitData[1] + " | " + splitData[2] + " | " + splitData[3] + " | " + splitData.Length);
+
+                values[0] = float.Parse(splitData[1]);
+                values[1] = float.Parse(splitData[2]);
+                values[2] = float.Parse(splitData[3]);
+            }
+
+            return values;
+
         }
     }
 }
