53b03a06f027d0229e5320fd5aab18561605c805
Patrick Smith <rippersmith61@gmail.com>
2024-06-01 11:38:16 -0400

    XR Tower Placement + XR UI Menu
    -downloaded a package for XR Hands UI Interaction
    -tower placement in XR scene now working as intended
    -have a working menu for XR UI interaction, other aspects of game previously seem to still be working (phase switching, tower & enemy AI, etc)

--- Assets/MiniMapTowerPlacement.cs
+++ Assets/MiniMapTowerPlacement.cs
@@ -5,35 +5,44 @@ using UnityEngine;
 public class MiniMapTowerPlacement : MonoBehaviour
 {
     [SerializeField] GameObject mainMap;
+    [SerializeField] private PlayerStats playerStats;
     PropDropping newProp;
+
+    private void Start()
+    {
+        playerStats = FindObjectOfType<PlayerStats>();
+    }
 
     public void DropNewProp(GameObject newProp, GameObject newMainMapTower, Vector3 localDropPoint)
     {
-        Debug.Log("Go Thru TTPTBM // Point: " + localDropPoint);
+        TowerBehaviour towerToDrop = newProp.GetComponent<PropDropping>().towerSpawn.GetComponent<TowerBehaviour>();
 
-        newProp.transform.parent = this.transform;
-        newProp.transform.rotation = Quaternion.Euler(Vector3.zero);
-        newProp.transform.position = localDropPoint;
-        newProp.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+        if (playerStats.CurrentMoney >= towerToDrop.towerCost)
+        {
+            Debug.Log("Go Thru TTPTBM // Point: " + localDropPoint);
 
-        RotateTowerTowardsPath(newProp);
+            newProp.transform.parent = this.transform;
+            newProp.transform.rotation = Quaternion.Euler(Vector3.zero);
+            newProp.transform.position = localDropPoint;
+            newProp.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
 
-        GameObject newTower = Instantiate(newMainMapTower, Vector3.zero, newProp.transform.rotation);
-        newTower.transform.parent = mainMap.transform;
-        newTower.transform.localPosition = newProp.transform.localPosition;
+            RotateTowerTowardsPath(newProp);
 
+            GameObject newTower = Instantiate(newMainMapTower, Vector3.zero, newProp.transform.rotation);
+            newTower.transform.parent = mainMap.transform;
+            newTower.transform.localPosition = newProp.transform.localPosition;
 
-
-        //GameObject bigTowerToSpawn = newProp.GetComponent<PropInfo>
+            PlaceNewTower(newTower, newTower.GetComponent<BoxCollider>());
+        }
     }
 
-    /*void PlaceNewTower(GameObject tower, Collider towerCollider, bool playTowerPlacedSFX = true)
+    void PlaceNewTower(GameObject tower, Collider towerCollider, bool playTowerPlacedSFX = true)
     {
         TowerBehaviour currentTowerBehaviour = tower.GetComponent<TowerBehaviour>();
         TowerDefenseManager.towersInGame.Add(currentTowerBehaviour);
 
-        if (currentPlacingTower != null)
-            RotateTowerTowardsPath(currentPlacingTower);
+        //if (currentPlacingTower != null)
+        //    RotateTowerTowardsPath(currentPlacingTower);
 
         playerStats.SubtractMoney(currentTowerBehaviour.towerCost);
 
@@ -49,7 +58,9 @@ public class MiniMapTowerPlacement : MonoBehaviour
             AudioManager.instance.PlaySFXArray("TowerPlaced", tower.transform.position);
 
         towerCollider.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-    }*/
+
+        Debug.Log("XR PLACEMENT SUCCESSFUL!");
+    }
 
     void RotateTowerTowardsPath(GameObject newTower)
     {
@@ -70,3 +81,178 @@ public class MiniMapTowerPlacement : MonoBehaviour
         }
     }
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*public class TowerPlacementVR : MonoBehaviour
+{
+    [SerializeField] private LayerMask placementCheckMask;
+    [SerializeField] private LayerMask placementCollideMask;
+    [SerializeField] private LayerMask obstaclePlacementCheckMask;
+    [SerializeField] private LayerMask obstaclePlacementCollideMask;
+    [SerializeField] private PlayerStats playerStats;
+
+    [SerializeField] private GameObject currentPlacingTower;
+    [SerializeField] GameObject radiusDecalObject;
+    //[SerializeField] RadiusSizeEditor radiusSizeEditor;
+    private Camera playerCamera;
+
+    [SerializeField] VRPointer placementPointer;
+    [SerializeField] ParticleSystem upgradeConfetti;
+    [SerializeField] public InputActionProperty towerSpawnerButton;
+    [SerializeField] public InputActionProperty cancelTowerSpawnButton;
+    [SerializeField] public InputActionProperty deleteTowerButton;
+
+    void Start()
+    {
+        playerCamera = Camera.main;
+    }
+
+    public void CancelTowerPlacement()
+    {
+        Destroy(currentPlacingTower);
+        currentPlacingTower = null;
+    }
+
+    void Update()
+    {
+        if (currentPlacingTower != null)
+        {
+            if (cancelTowerSpawnButton.action.WasPerformedThisFrame())
+            {
+                CancelTowerPlacement();
+                return;
+            }
+
+            currentPlacingTower.transform.position = placementPointer.endPoint;
+            radiusDecalObject.transform.position = placementPointer.endPoint;
+
+            if (towerSpawnerButton.action.WasPerformedThisFrame() && placementPointer.collision != null)
+            {
+                if (currentPlacingTower.CompareTag("Tower") &&
+                    !placementPointer.collision.CompareTag("Path") &&
+                    !placementPointer.collision.CompareTag("Tower"))
+                {
+                    BoxCollider towerCollider = currentPlacingTower.gameObject.GetComponent<BoxCollider>();
+                    towerCollider.isTrigger = true;
+
+                    Vector3 boxCenter = currentPlacingTower.gameObject.transform.position + towerCollider.center;
+                    Vector3 halfExtents = towerCollider.size / 2;
+
+                    if (!Physics.CheckBox(boxCenter, halfExtents, Quaternion.identity, placementCheckMask, QueryTriggerInteraction.Ignore))
+                    {
+                        CreateNewTower(currentPlacingTower, towerCollider);
+                        currentPlacingTower = null;
+
+                        radiusDecalObject.transform.position = new Vector3(0f, -1000f, 0f);
+                    }
+                }
+                else if ((currentPlacingTower.CompareTag("Obstacle") ||
+                        currentPlacingTower.CompareTag("AttackObstacle")) &&
+                        placementPointer.collision.CompareTag("Path"))
+                {
+                    BoxCollider towerCollider = currentPlacingTower.gameObject.GetComponent<BoxCollider>();
+                    towerCollider.isTrigger = true;
+
+                    Vector3 boxCenter = currentPlacingTower.gameObject.transform.position + towerCollider.center;
+                    Vector3 halfExtents = towerCollider.size / 2;
+
+                    if (!Physics.CheckBox(boxCenter, halfExtents, Quaternion.identity, obstaclePlacementCheckMask, QueryTriggerInteraction.Ignore))
+                    {
+                        TowerBehaviour currentTowerBehaviour = currentPlacingTower.GetComponent<TowerBehaviour>();
+                        TowerDefenseManager.towersInGame.Add(currentTowerBehaviour);
+
+                        playerStats.SubtractMoney(currentTowerBehaviour.towerCost);
+
+                        //towerCollider.isTrigger = false;
+                        towerCollider.gameObject.layer = 9;
+                        foreach (Transform child in towerCollider.transform)
+                        {
+                            child.gameObject.layer = 9;
+                        }
+                        AudioManager.instance.PlaySFXArray("TowerPlaced", currentPlacingTower.transform.position);
+                        towerCollider.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+                        currentPlacingTower = null;
+
+                        radiusDecalObject.transform.position = new Vector3(0f, -1000f, 0f);
+                    }
+                }
+            }
+        }
+
+        else
+        {
+            if (deleteTowerButton.action.WasPerformedThisFrame() && placementPointer.collision != null)
+            {
+                Debug.Log("ButtonPressed");
+                TowerBehaviour deletingTower = placementPointer.collision.GetComponent<TowerBehaviour>();
+                if (deletingTower != null && (deletingTower.gameObject.CompareTag("Tower") || deletingTower.gameObject.CompareTag("Obstacle") || deletingTower.gameObject.CompareTag("AttackObstacle")))
+                {
+                    Debug.Log("DeletingTower");
+                    playerStats.AddMoney(deletingTower.towerCost);
+                    TowerDefenseManager.EnqueueTowerToRemove(deletingTower);
+                }
+
+            }
+        }
+
+    }
+
+    public void SetTowerToPlace(GameObject newTower)
+    {
+        if (TowerDefenseManager.CurrPhase == Phase.Build ||
+            TowerDefenseManager.CurrPhase == Phase.Repair)
+        {
+            int newTowerCost = newTower.GetComponent<TowerBehaviour>().towerCost;
+
+            if (playerStats.CurrentMoney >= newTowerCost)
+            {
+                if (currentPlacingTower != null)
+                {
+                    CancelTowerPlacement();
+                }
+
+                currentPlacingTower = Instantiate(newTower, Vector3.zero, Quaternion.identity);
+                RadiusSizeEditor.instance.ChangeRadiusSize(newTower.GetComponent<TowerBehaviour>());
+            }
+            else
+            {
+                Debug.Log("NOT_ERROR: You do not have the appropriate funds to buy this tower!");
+            }
+        }
+        else { Debug.LogError("ERROR: You are not currently in the Build or Repair phase!"); }
+    }
+
+    public void UpgradeTower(GameObject oldTower, GameObject upgradedTower)
+    {
+        if (TowerDefenseManager.CurrPhase == Phase.Build ||
+            TowerDefenseManager.CurrPhase == Phase.Repair)
+        {
+            int newTowerCost = upgradedTower.GetComponent<TowerBehaviour>().towerCost;
+
+            if (playerStats.CurrentMoney >= newTowerCost)
+            {
+                GameObject newTower = Instantiate(upgradedTower, oldTower.transform.position, oldTower.transform.rotation);
+                BoxCollider towerCollider = newTower.GetComponent<BoxCollider>();
+
+                TowerDefenseManager.towersInGame.Remove(oldTower.GetComponent<TowerBehaviour>());
+
+                CreateNewTower(newTower, towerCollider, false);
+                Destroy(oldTower);
+
+                AudioManager.instance.PlaySFXArray("TowerUpgrade", newTower.transform.position);
+
+                upgradeConfetti.transform.position = newTower.transform.position;
+                upgradeConfetti.Play();
+            }
+            else
+            {
+                Debug.Log("UPGRADE_ERROR: You do not have the appropriate funds to upgrade this tower!");
+            }
+        }
+        else { Debug.LogError("ERROR: You are not currently in the Build or Repair phase!"); }
+    }
+}
+*/
