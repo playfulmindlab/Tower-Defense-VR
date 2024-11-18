15cb942abde3eb37596791d7a59234f699086adf
Patrick Smith <rippersmith61@gmail.com>
2024-11-18 08:44:51 -0500

    Body Y Repositioning + Electric Beam Upgrade Disabled
    -"upgrade" button on electric beam prop is now disabled
    -body now follows head on the Y position as well as X & Z

--- Assets/FollowVRCamera.cs
+++ Assets/FollowVRCamera.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 public class FollowVRCamera : MonoBehaviour
 {
     [SerializeField] Transform playerCamera;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float yDiff = -5f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(playerCamera.position.x, transform.position.y, playerCamera.position.z);
+        transform.position = playerCamera.position + (Vector3.up * yDiff);//new Vector3(playerCamera.position.x, transform.position.y, playerCamera.position.z);
         transform.rotation = Quaternion.Euler(new Vector3(0f, playerCamera.rotation.eulerAngles.y, 0f));
     }
 }
